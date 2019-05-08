6d0343d48d675378b32959ac53077944f4e9c79e
John Moo-King <jmooking@hotmail.com>
2019-05-08 17:12:37 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,9 +16,13 @@ def turn(board)
   else
     turn(board)
   end
-
+  display_board(board)
 end
 
 def input_to_index (user_input)
   user_input.to_i - 1
+end
+
+def move (board, index, character = "X")
+  board[index] = character
 end
